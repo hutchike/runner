5afd726d478bdac43ab455a10b112a06bb387a2e
Kevin Hutchinson <kevin.hutchinson@legendum.com>
2011-04-29 23:52:02 -0500

    added meta desc

--- app/views/en/types/html.php
+++ app/views/en/types/html.php
@@ -1,6 +1,7 @@
 <html>
 <head>
     <title><?= $title ?></title>
+    <meta name="description" content="Calculates run times, speeds and distances" />
     <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
     <link rel="shortcut icon" href="/favicon.ico" />
 </head>
